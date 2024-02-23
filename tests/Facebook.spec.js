f0f71b269116b9d6f13b8577072cfa9a645712b6
Plesirima <160445892+Plesirima@users.noreply.github.com>
2024-02-23 15:27:06 +0700

    [Modified] Add Step

--- tests/Facebook.spec.js
+++ tests/Facebook.spec.js
@@ -2,25 +2,27 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.facebook.com/');
+  // Step 1 :: go to search Facebook.
+  await test.step('Step 1 :: go to search page', async () => {
+   await page.goto('https://www.facebook.com/');
+  });
 
-  // Expect a title "to contain" a substring.
-  const seachFieldUSER = page.locator('#email');
-  const seachFieldPW = page.locator('#pass');
+  // Step 2 :: Login Facebook.
+  await test.step('Step 2 :: Login Facebook', async () => {
+   const seachFieldUSER = page.locator('#email');
+   const seachFieldPW = page.locator('#pass');
+   await seachFieldUSER.fill('zimplesirima@gmail.com');
+   await seachFieldPW.fill('zimple@sirima');
+   await page.getByRole('button', { name: 'Log in' }).click();
+  });
 
-  await seachFieldUSER.fill('zimplesirima@gmail.com');
-  await seachFieldPW.fill('zimple@sirima');
-
-    // Click the Login
-    await page.getByRole('button', { name: 'Log in' }).click();
-    // Click the Home
+    // Step 2 :: Post on Facebook.
+  await test.step('Step 3 :: Post on Facebook', async () => {
     await page.getByRole('link', { name: 'Facebook' }).click();
-     // Click the POST
-     await page.getByRole('button', { name: "What's on your mind, สิริมา?" }).click();
-
-    await page.getByLabel('What\'s on your mind, สิริมา?').fill('Hello Playwright');
+    await page.getByRole('button', { name: "What's on your mind, สิริมา?" }).click();
+    await page.getByLabel('What\'s on your mind, สิริมา?').fill('Hello Playwright!');
     await page.getByLabel('Post', {exact: true} ).press('Enter');
-
+  });
 
 });
 
